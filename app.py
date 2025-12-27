253184de4fa0152a266d05daf755d657b155ef95
sushikid90-coder <sushikid90@gmail.com>
2025-12-27 11:07:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -298,7 +298,8 @@ def admin():
     return render_template_string(ADMIN, ok=ok, rows=rows, key=key)
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
