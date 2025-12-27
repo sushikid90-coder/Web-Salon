3198f0dc0ffd3ce1e6e3c187592f3457c303aff5
sushikid90-coder <sushikid90@gmail.com>
2025-12-27 10:31:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -359,7 +359,6 @@ PAGE = """
 </body>
 </html>
 """
- """
 <!doctype html>
 <html>
 <head>
@@ -452,6 +451,7 @@ if _name_ == "_main_":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
